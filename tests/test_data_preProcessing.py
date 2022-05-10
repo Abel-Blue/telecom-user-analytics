401253248c0098103b7af82d053ce5c7cd8fa74d
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-10 17:45:37 +0300

    add modules to test

---
+++ tests/test_data_preProcessing.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+import numpy as np
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join('scripts')))
+
+from data_preProcessing import data_preProcessing_script
