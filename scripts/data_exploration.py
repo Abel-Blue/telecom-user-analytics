8e76773fb9f8d61cf05acc104e0c01ceae1e7cdb
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-11 00:02:17 +0300

    added scatter plot functionality

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -14,11 +14,13 @@ class exploration:
 
 
     def plot_heatmap(df: pd.DataFrame, title: str, cbar=False) -> None:
-        # heatmap: Plot rectangular data as a color-encoded matrix.
-        # heatmap of the dataframe
-        # cbar: Whether to draw a colorbar.
-        # title: Title of the plot
-        # df: dataframe to be plotted
+        ''' 
+        heatmap: Plot rectangular data as a color-encoded matrix.
+        heatmap of the dataframe
+        cbar: Whether to draw a colorbar.
+        title: Title of the plot
+        df: dataframe to be plotted
+        '''
 
         plt.figure(figsize=(13, 8))
         sns.heatmap(df, annot=True, cmap='viridis', vmin=0,
@@ -27,13 +29,30 @@ class exploration:
         plt.show()
     
     def plot_heatmap_from_correlation(correlation, title: str):
-        # heatmap: Plot rectangular data as a color-encoded matrix and correlation matrix.
-        # title: Title of the plot
-        # correlation: correlation matrix
-        
+        '''
+        heatmap: Plot rectangular data as a color-encoded matrix and correlation matrix.
+        title: Title of the plot
+        correlation: correlation matrix
+        '''
         plt.figure(figsize=(19, 15))
         sns.heatmap(correlation)
         plt.title(title, size=18, fontweight='bold')
+        plt.show()
+
+    def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
+        '''
+        # scatter: Plot data as a scatter plot.
+        # df: dataframe to be plotted
+        # x_col: x-axis column
+        # y_col: y-axis column
+        # title: Title of the plot
+        # hue: hue column
+        '''
+        plt.figure(figsize=(12, 7))
+        sns.scatterplot(data=df, x=x_col, y=y_col, hue=hue, style=style)
+        plt.title(title, size=20)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
         plt.show()
 
 
